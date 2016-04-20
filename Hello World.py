245fd3b12b364cc8486782d42f663d6b7ee9966e
leo <leo_advance@163.com>
2016-04-20 22:19:45 +0800

    增加QT测试

--- Hello World.py
+++ Hello World.py
@@ -1,5 +1,14 @@
 import datetime
 
+import sys
+from PyQt5 import QtWidgets, QtCore
 print('Hello')
 print('Hi Python this is Leo')
 print('Now is',datetime.datetime.now(),'We Right the first code')
+
+app = QtWidgets.QApplication(sys.argv)
+widget = QtWidgets.QWidget()
+widget.resize(360, 360)
+widget.setWindowTitle("Hello, PyQt5!")
+widget.show()
+sys.exit(app.exec_())
