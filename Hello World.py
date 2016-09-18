e3d8101406727fecd83901c11c8a84f6c661ded3
leoadvance <leo_advance@163.com>
2016-09-19 00:26:51 +0800

    充实Math类

--- Hello World.py
+++ Hello World.py
@@ -3,52 +3,22 @@
 # 导入模块 datetime sys 以及Qt
 import datetime
 import sys
-from MathStudy import GrandPa
+from MathStudy import MathStudy
 
 #from PyQt5 import QtWidgets
 
 
-aa = GrandPa()
-aa.StartTest()
+aa = MathStudy()
+aa.type_study()
+aa.math_study()
 
-#--------------------------------- 数据类型测试 ----------------------------------
-print ("-------------------------- 数据类型测试演示 ----------------------------")
-a = 10
-b = 1.1
-c = True
-d = '221'
 
-# int型
-print ("a = 10,    a = ", type(a))
 
-# float型
-print ("b = 1.1,   b = ", type(b))
 
-# bool型
-print ("c = True,  c = ", type(c))
-
-# str型
-print ("d = '221', d = ", type(d))
-
-# 字符串变整型
-print(int(d) + a)
-
-# 整型变字符串
-print(str(a) + d)
-
-#--------------------------------- 数学操作符 -----------------------------------
-print ("--------------------------- 数学操作符演示 -----------------------------")
-print ("指数 2  ** 3 =", 2 ** 3)
-print ("取余 22 %  3 =", 22 % 3)
-print ("整除 22 // 3 =", 22 // 3)
-print ("除法 22 /  3 =", 22 / 3)
-print ("乘法 22 *  3 =", 22 * 3)
-print ("加法 22 +  3 =", 22 + 3)
-print ("减法 22 -  3 =", 22 - 3)
 
 #------------------------- dir 查看模块，变量属性和方法 ---------------------------
 
-print(dir(a))
+print(dir(10))
 
 #*******************************************************************************
 #                           陆超@2016-09-17
--- MathStudy.py
+++ MathStudy.py
@@ -1,6 +1,65 @@
 
-class GrandPa:
+class MathStudy:
 
-    def StartTest(self):
+#*******************************************************************************
+#                           陆超@2016-09-17
+# Function Name  :  type_study
+# Description    :  数据类型测试
+# Input          :  self 类实体
+# Output         :  None
+# Return         :  None
+#*******************************************************************************
 
-        print("Im GrandPa")
+    def type_study(self):
+
+        print(self)
+        print(self.__class__)
+
+
+        print(
+            "-------------------------- 数据类型测试演示 ----------------------------")
+        a = 10
+        b = 1.1
+        c = True
+        d = '221'
+
+        # int型
+        print("a = 10,    a = ", type(a))
+
+        # float型
+        print("b = 1.1,   b = ", type(b))
+
+        # bool型
+        print("c = True,  c = ", type(c))
+
+        # str型
+        print("d = '221', d = ", type(d))
+
+        # 字符串变整型
+        print(int(d) + a)
+
+        # 整型变字符串
+        print(str(a) + d)
+
+#*******************************************************************************
+#                           陆超@2016-09-17
+# Function Name  :  math_study
+# Description    :  数学操作符测试
+# Input          :  self 类实体
+# Output         :  None
+# Return         :  None
+#*******************************************************************************
+
+    def math_study(self):
+        print(self)
+        print(self.__class__)
+
+        print(
+            "--------------------------- 数学操作符演示 -----------------------------")
+        print("指数 2  ** 3 =", 2 ** 3)
+        print("取余 22 %  3 =", 22 % 3)
+        print("整除 22 // 3 =", 22 // 3)
+        print("除法 22 /  3 =", 22 / 3)
+        print("乘法 22 *  3 =", 22 * 3)
+        print("加法 22 +  3 =", 22 + 3)
+        print("减法 22 -  3 =", 22 - 3)
