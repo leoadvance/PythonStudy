ba2450efae8db8b1dec88fb3db1acf3a7a7cbef5
leoadvance <leo_advance@163.com>
2016-12-10 16:46:35 +0800

    增加对嵌套目录的支持

--- Shell.py
+++ Shell.py
@@ -10,6 +10,75 @@ import subprocess
 class Shell(object):
 
 #*******************************************************************************
+#                           陆超@2016-12-10
+# Function Name  :  turnToH265
+# Description    :  转换目录下文件至本目录下H265文件夹
+# Input          :  self 类实体
+# Output         :  None
+# Return         :  None
+#*******************************************************************************
+    def turnToH265(FindPath):
+
+        # 目标目录
+        H265Directory = FindPath + "/H265"
+
+        FileNames     = os.listdir(FindPath)
+        for i in FileNames:
+
+            # 获取文件路径
+            Path = os.path.join(FindPath, i)
+
+            # 是目录
+            if os.path.isdir(Path):
+
+                print("文件夹：" + Path);
+                Shell.turnToH265(Path)
+
+            # 判断后缀并输出输出文件名
+            elif os.path.splitext(i)[1] == ".mkv" or os.path.splitext(i)[1] == ".mp4" or os.path.splitext(i)[1] == ".mov" or os.path.splitext(i)[1] == ".ts":
+                '''
+                # 创建转换后目录
+                if os.path.exists(H265Directory) == False:
+                    os.mkdir(H265Directory)
+                    print("创建目录：" + H265Directory)
+                    '''
+                # 文件名
+                filename     = os.path.join(i)
+
+                # 文件与路径名
+                fullfilename = os.path.join(FindPath, i)
+
+                # 分离文件名和扩展
+                filenameHead, filenamExt = os.path.splitext(filename)
+                print("文件名：" + filename + " 文件头:" + filenameHead + " 扩展名:" + filenamExt)
+
+                # 替换空格
+                filename          = filename.replace(' ', '\ ')
+                filenameHead      = filenameHead.replace(' ', '\ ')
+                fullfilenameshell = fullfilename.replace(' ', '\ ')
+                # print(filename)
+                # print(fullfilename)
+
+
+
+                # 组合成想要的命名
+                shellcmd = ("ffmpeg -i " + fullfilenameshell +
+                            " -c:v libx265 -preset medium -crf 23 -c:a aac -b:a 384k " +
+                            FindPath + filenameHead + ".H265.mp4")
+
+                print(shellcmd)
+                os.system(shellcmd)
+                
+                '''
+                # 创建该文件
+                newFilePath = FindPath + "/" + filenameHead + ".H265.mp4"
+                f = open(newFilePath, 'w')
+                f.close();
+                print("创建新文件：" + newFilePath)
+                '''
+        return
+
+#*******************************************************************************
 #                           陆超@2016-12-06
 # Function Name  :  Test
 # Description    :  shell学习
@@ -24,38 +93,5 @@ class Shell(object):
         print(self.__class__)
 
         # 遍历目录下文件
-        FindPath = "/Users/LEO/Desktop"
-        FileNames=os.listdir(FindPath)
-        print(FileNames)
-        for i in FileNames:
-            # 判断后缀并输出输出文件名
-            if os.path.splitext(i)[1] == ".mkv" or os.path.splitext(i)[1] == ".mp4":
-
-                # 文件名
-                filename     = os.path.join(i)
-
-                # 文件与路径名
-                fullfilename = os.path.join(FindPath, i)
-
-                # 替换空格
-                filename          = filename.replace(' ', '\ ')
-                fullfilenameshell = fullfilename.replace(' ', '\ ')
-                # print(filename)
-                # print(fullfilename)
-
-                # 组合成想要的命名
-                shellcmd = "ffmpeg -i " + fullfilenameshell + " -c:v libx265 -preset medium -crf 28 -c:a aac -b:a 384k " + FindPath + "/H265/" + filename
-
-                if "mkv" in shellcmd:
-                    # 删除多余.mkv
-                    shellcmd = shellcmd.rstrip(".mkv")
-                    shellcmd += ".mp4"
-                print(shellcmd)
-                os.system(shellcmd)
-
-        a = "ls"
-        b = " -l"
-        c = a + b
-        subprocess.call(c, shell=True)
-        print(
-            "-------------------------- shell测试 ----------------------------")
+        FindPath  = "/Users/LEO/Desktop"
+        Shell.turnToH265(FindPath)
