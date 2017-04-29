f034e0e556e9be5ce71403125a43f5e7c123b3dc
leoadvance <leo_advance@163.com>
2017-04-29 16:23:08 +0800

    增加串口路径和关键词定义

--- SensorSave&Upload/Sensor.py
+++ SensorSave&Upload/Sensor.py
@@ -7,17 +7,21 @@ import time
 import os;
 import sys;
 
+
+#------------------------------ 宏定义 -----------------------------------------#
+serialFindPath      = '/dev'                                            # 串口查找目录
+serialFindKeyword   = 'usbserial'                                       # 串口查找关键词
+
 #*******************************************************************************
 #                           陆超@2017-04-29
-# Function Name  :  isDevExist
+# Function Name  :  isFileExist
 # Description    :  判断某文件是否存在
 # Input          :  filePath    查找的路径
 #                   keyWord     关键字
 # Output         :  None
 # Return         :  成功返回文件名  不存在 0
 #*******************************************************************************
-
-def isDevExist(filePath, keyWord):
+def isFileExist(filePath, keyWord):
 
     # 执行shell命令
     output = os.popen(filePath)
@@ -30,16 +34,32 @@ def isDevExist(filePath, keyWord):
             return i.strip()
     return 0
 
+#*******************************************************************************
+#                           陆超@2017-04-29
+# Function Name  :  isSerialExist
+# Description    :  判断串口是否存在
+# Input          :  None
+# Output         :  None
+# Return         :  成功返回文件名  退出结束程序
+#*******************************************************************************
+def isSerialExist(serialFindPath, serialFindKeyword):
+
+    # 阻止命令
+    serialFindPath = 'ls ' + serialFindPath
+    serialPath     = isFileExist(serialFindPath, serialFindKeyword)
+    if ( serialPath != 0):
+        return serialPath
+    else :
+        print("没找到串口，程序结束！")
+        sys.exit(0)
+
+
 print('Get Sensor Data and upload')
 
 
 # 判断串口是否存在
-serialPath = isDevExist('ls /dev', 'usbserial')
-if ( serialPath != 0):
-    print(serialPath)
-else :
-    print("没找到串口，程序结束！")
-    sys.exit(0)
+serialPath = isSerialExist(serialFindPath, serialFindKeyword)
+print(serialPath)
 
 while 1 :
     print('Now is', datetime.datetime.now())
