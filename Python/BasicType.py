9de94bd9c99dc28a70d558552161c7967ee6ce72
leoadvance <leo_advance@163.com>
2017-09-27 01:12:12 +0800

    1. 增加Print_Fun函数。 2. 学习定义编码格式#coding=utf-8 3. 学会import 文件名来引用外部函数。

--- Python/BasicType.py
+++ Python/BasicType.py
@@ -1,4 +1,5 @@
-
+#coding=utf-8
+# 定义编码格式
 
 #Numbers（数字）
 #String（字符串）
@@ -6,5 +7,12 @@
 #Tuple（元组）
 #Dictionary（字典）
 
+# 通过import文件名引入其他文件内函数
+import Print
+
+
+
 via1 = 1
+# print必须带括号
 print (via1)
+Print.Print_Fun()
---
+++ Python/Print.py
@@ -0,0 +1,7 @@
+#codingutf-8
+
+
+# 函数以def开头 参数可省略 以冒号结尾 return返回
+def Print_Fun():
+    print ("Print 函数测试！")
+    return
