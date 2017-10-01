5e72003004ac91fa361f7c3831a2b1c6b3432a89
leo_advance <leo_advance@163.com>
2017-10-01 23:28:41 +0800

    学习list列表使用

--- Python/Variable.py
+++ Python/Variable.py
@@ -5,8 +5,29 @@ def VariableTest():
     # 声明字符串变量s
     s = "123456"
 
-    print ("字符串变量测试:")
+    print ("\r\n字符串变量测试:")
     print ("print (s): " + s)
     print ("print (s[1:5]): " + s[1:5])
+
+    # 变量可以直接通过加连接
+    s += "hello"
+    print(s)
+
+    # 变量可以通过乘法倍增
+    s *= 3
+    print(s)
+
+
+    # 列表
+    print ("\r\nlist列表测试:")
+    list = ["234", 768476, 12.3, "Test"]
+    print("print (list)")
+    print (list)
+    print("print (list[1:3])")
+    print (list[1:3])
+    print("list *= 2")
+    list *= 2
+    print("print (list)")
+    print(list)
 
     return
