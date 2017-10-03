cd3155ac8b0757ae0219c8690aeefc01eb296012
leoadvance <leo_advance@163.com>
2017-10-03 13:16:02 +0800

    学习指数运算

--- Python/Math.py
+++ Python/Math.py
@@ -9,11 +9,17 @@ def MathTest():
     print ("a = 0x%x"%(a))
     print ("a = " + hex(a) + " " + "a = " + oct(a) + " " + "a = " + bin(a))
 
-    print ("取余数运算 % （5.1 % 2.1）")
+    print ("\r\n取余数运算 % （5.1 % 2.1）")
     a = 5.1
     b = 2.1
     print (a % b)
 
-    print("取整运算 % （5.1 // 2.1）")
+    print("\r\n取整运算 % （5.1 // 2.1）")
     print (a // b)
+
+    print("\r\n指数运算 % （2 ** 3）")
+    a = 2
+    b = 3
+    print (a ** b)
+
     return
