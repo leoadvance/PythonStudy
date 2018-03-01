d90e503a51a5920e2c951178699ce1f4e2f20308
leoadvance <leo_advance@163.com>
2018-03-01 18:45:06 +0800

    爬取第一帧数据

--- Python/BasicType.py
+++ Python/BasicType.py
@@ -22,7 +22,7 @@ import MultiProcess
 import myThread
 import Qt5
 import Algorithm
-
+import re
 import itchat
 import urllib3
 import requests
@@ -77,36 +77,16 @@ Print.Print_Var(1,'123', 3)
 # 算法学习
 # classAlgorithm = Algorithm.Algorithm()
 
+page = 3300
+while 1:
+    url = "http://api.wallstreetcn.com/v2/livenews"
+    # get参数
+    data = {
+        "page": page
+    }
+    content = requests.get(url=url, params=data).content
 
-url='https://www.zhihu.com/question/22591304/followers'
-#感觉这个话题下面美女多
-headers={"美女"}
-i=1
-for x in range(20,3600,20):
-    data = {'start':'0','offset':str(x),
-          '_xsrf':'a128464ef225a69348cef94c38f4e428'}
-    #知乎用offset控制加载的个数，每次响应加载20
-    content = requests.post(url,headers=headers,data=data,timeout=10).text
-    #用post提交form data
-    imgs = re.findall('<img src=\\\\\"(.*?)_m.jpg',content)
-    #在爬下来的json上用正则提取图片地址，去掉_m为大图
-    for img1 in imgs:
-        try:
-            img1 = img.replace('\\','')
-            #去掉\字符这个干扰成分
-            pic=img1+'.jpg'
-            path='d:\\bs4\\zhihu\\jpg\\'+str(i)+'.jpg'
-            #声明存储地址及图片名称
-            urllib3.urlretrieve(pic,path)
-            #下载图片
-            print ('下载了第'+str(i)+'张图片')
-            i+=1
-            sleep(random.uniform(0.5,1))
-        #睡眠函数用于防止爬取过快被封IP
-        except:
-            print ('抓漏1张')
-            pass
-    sleep(random.uniform(0.5,1))
+    print(content)
 
 #myThread.threadTest()
 print ("Good Bye 2017 & Welcome 2018")
