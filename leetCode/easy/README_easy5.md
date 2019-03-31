ea8804e5cd497f6de1927ff0ab4f42a1a6f946d7
leoadvance <leo_advance@163.com>
2019-03-31 14:01:06 +0800

    解决1029. Binary Prefix Divisible By 5

--- leetCode/easy/README_easy5.md
+++ leetCode/easy/README_easy5.md
@@ -6,3 +6,8 @@
     1. 要求最大周长，先对数组进行降序排列并遍历。
     2. 通过两边之和大于第三边来判断三角形。 
 
+
+* [# 1029. Binary Prefix Divisible By 5 (prefixesDivBy5)](https://leetcode.com/problems/binary-prefix-divisible-by-5/)  
+题目需求一个数据是否能被5整除。   
+要点：
+    1. 利用除法特性，直接用余数迭代。
--- leetCode/easy/easy5.py
+++ leetCode/easy/easy5.py
@@ -4,10 +4,23 @@ import time
 import collections
 import operator
 
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 class SolutionEasy():
 
     def __init__(self):
-
+        self.list = ListNode(1)
+        self.list.next = ListNode(7)
+        self.list.next.next = ListNode(5)
+        self.list.next.next.next = ListNode(1)
+        self.list.next.next.next.next = ListNode(9)
+        self.list.next.next.next.next.next = ListNode(2)
+        self.list.next.next.next.next.next.next = ListNode(5)
+        self.list.next.next.next.next.next.next.next = ListNode(1)
         print("class leetCode SolutionEasy init!")
 
 
@@ -33,12 +46,78 @@ class SolutionEasy():
         print(0)
         return 0
 
+    # 判断一个数字是否被5整除
+    def prefixesDivBy5(self, A: list) -> list:
+        print("func largestPerimeter")
+        # print("    Sloution1:")
+        # print("        Runtime: 112 ms, faster than 100.00% of Python3 online submissions for Binary Prefix Divisible By 5.")
+        # print("        Memory Usage: 16.2 MB, less than 100.00% of Python3 online submissions for Binary Prefix Divisible By 5.")
+        outList = []
+        sum = 0
+        for data in A:
+            # 利用除法特性 直接用余数迭代
+            sum = ((sum <<1 ) + data) % 5
+            if sum == 0:
+                outList.append(True)
+            else:
+                outList.append(False)
+
+        # print(outList)
+        return outList
+
+    #
+    def baseNeg2(self, N: int) -> str:
+
+        # 4的整倍数 直接取二进制
+        strs = bin(N)[2:]
+
+        totalLan = len(strs)
+        i = 0
+        while i < totalLan:
+            # -2的奇数次方 是负数
+            if i % 2 == 1 and strs[0 - i - 1] == "1":
+                N += 2 ** (i + 1)
+                strs = bin(N)[2:]
+                totalLan = len(strs)
+            i += 1
+            # print(i , strs)
+        # else:
+
+        print(strs)
+        return strs
+
+    # 求list里每个数字下一个最大值
+    def nextLargerNodes(self, head: ListNode) -> list:
+
+        # listNode转list
+        templist = []
+        while head:
+            templist.append(head.val)
+            head = head.next
+        print(templist)
+        # 遍历 求解每个数组右边最大值位置 最后一位右边无最大值
+        outList = []
+        for i in range(len(templist)):
+            outList.append(0)
+            for j in range(i + 1, len(templist)):
+                if templist[j] > templist[i]:
+                    outList[i] = templist[j]
+                    break
+
+        # print(dictTemp)
+        print(outList)
+        return outList
+        pass
+
+
     def run(self):
 
         startTime = time.time()
 
-        self.largestPerimeter([3,6,2])
-
+        # self.largestPerimeter([3,6,2])
+        # self.prefixesDivBy5([0,1,1,1,1,1])
+        # self.baseNeg2(13)
+        self.nextLargerNodes(self.list)
         endTime = time.time()
 
         print("run time: ", (str(endTime - startTime))[:8], "s")
